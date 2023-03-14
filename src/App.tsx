470dc3cb53d701d363e4d9d46c2a5e904b0c0a64
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2023-03-14 14:59:56 +0300

    navbar and wrapper components

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,33 @@ import Background from "./components/Background";
 import Footer from "./sections/Footer";
 import Navbar from "./sections/Navbar";
 import Wrapper from "./sections/Wrapper";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./scss/index.scss";
+import Search from "./pages/Search";
+import MyList from "./pages/MyList";
+import About from "./pages/About";
+import Compare from "./pages/Compare";
+import Pokemon from "./pages/Pokemon";
 
 function App() {
   return (
     <div className="main-container">
       <Background />
-      <div className="app">
-        <Navbar />
-        <Wrapper />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route element={<Search />} path="/search" />
+            <Route element={<MyList />} path="/list" />
+            <Route element={<About />} path="/about" />
+            <Route element={<Compare />} path="/compare" />
+            <Route element={<Pokemon />} path="/pokemon/:id" />
+            <Route element={<Navigate to="/pokemon/1" />} path="*" />
+          </Routes>
+  
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
