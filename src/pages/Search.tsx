d80745094ed4c8a1973b0232aeeef3c5c8b5f633
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2023-03-15 15:38:32 +0300

    search function

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -4,6 +4,7 @@ import { getInitialPokemonData } from "../app/reducers/getInitialPokemonData";
 import { getPokemonData } from "../app/reducers/getPokemonData";
 import PokemonCardGrid from "../components/PokemonCardGrid";
 import Wrapper from "../sections/Wrapper";
+import { debounce } from "../utils/Debounce";
 
 function Search() {
   const dispatch = useAppDispatch();
@@ -25,6 +26,23 @@ function Search() {
     }
   }, [allPokemon, dispatch]);
 
+  const handleChange = debounce((value: string) => getPokemon(value), 300);
+
+  const getPokemon = async (value: string) => {
+    if (value.length) {
+      const pokemons = allPokemon?.filter((pokemon) =>
+        pokemon.name.includes(value.toLowerCase())
+      );
+      dispatch(getPokemonData(pokemons!));
+    } else {
+      const clonedPokemons = [...(allPokemon as [])];
+      const randomPokemonsId = clonedPokemons
+        .sort(() => Math.random() - Math.random())
+        .slice(0, 20);
+      dispatch(getPokemonData(randomPokemonsId));
+    }
+  };
+
   return (
     <>
       <div className="search">
@@ -32,6 +50,7 @@ function Search() {
           type="text"
           className="pokemon-searchbar"
           placeholder="Search Pokemon"
+          onChange={(e) => handleChange(e.target.value)}
         />
         <PokemonCardGrid pokemons={randomPokemons!} />
       </div>
