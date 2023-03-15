aa1947bf57e470f27e0785576d4edde14be1a919
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2023-03-15 11:17:49 +0300

    getPokememonImages , getpokemontypes, getpokemonstdata

--- src/app/reducers/getPokemonData.ts
+++ src/app/reducers/getPokemonData.ts
@@ -1,6 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { generatedPokemonType, genericPokemonType } from "../../utils/Types";
 import axios from "axios";
+import { defaultImages, images } from "../../utils/getPokemonImages";
+import { pokemonTypes } from "../../utils/getPokemonTypes";
 
 export const getPokemonData = createAsyncThunk(
   "pokemon/randomPokemon",
@@ -8,8 +10,38 @@ export const getPokemonData = createAsyncThunk(
     try {
       const pokemonsData: generatedPokemonType[] = [];
       for await (const pokemon of pokemons) {
-        const { data } = await axios.get(pokemon.url);
-        console.log(data);
+        const {
+          data,
+        }: {
+          data: {
+            id: number;
+            types: { type: genericPokemonType }[];
+          };
+        } = await axios.get(pokemon.url);
+        const types = data.types.map(
+          ({ type: { name } }: { type: { name: string } }) => ({
+            //@ts-expect-error
+            [name]: pokemonTypes[name],
+          })
+        );
+        //@ts-expect-error
+        let image: string = images[data.id];
+
+        if (!image) {
+          //@ts-expect-error
+          image = defaultImages[data.id];
+        }
+
+        if (image) {
+          pokemonsData.push({
+            name: pokemon.name,
+            id: data.id,
+            image,
+            types,
+          });
+        }
+
+        console.log({ pokemonsData });
       }
     } catch (error) {}
   }
