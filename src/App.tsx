a584562ced5fe478e2056260199e2bebc15e258d
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2023-03-15 16:48:31 +0300

    add compare function and toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Background from "./components/Background";
 import Footer from "./sections/Footer";
 import Navbar from "./sections/Navbar";
@@ -9,8 +9,31 @@ import MyList from "./pages/MyList";
 import About from "./pages/About";
 import Compare from "./pages/Compare";
 import Pokemon from "./pages/Pokemon";
+import { ToastContainer, ToastOptions, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { clearToasts } from "./app/slices/AppSlice";
 
 function App() {
+  const { toasts } = useAppSelector(({ app }) => app);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (toasts.length) {
+      const toastOptions: ToastOptions = {
+        position: "bottom-right",
+        autoClose: 2000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+      };
+      toasts.forEach((message: string) => {
+        toast(message, toastOptions);
+      });
+      dispatch(clearToasts());
+    }
+  }, [toasts, dispatch]);
+
   return (
     <div className="main-container">
       <Background />
@@ -25,8 +48,8 @@ function App() {
             <Route element={<Pokemon />} path="/pokemon/:id" />
             <Route element={<Navigate to="/pokemon/1" />} path="*" />
           </Routes>
-  
           <Footer />
+          <ToastContainer />
         </div>
       </BrowserRouter>
     </div>
--- src/components/PokemonCardGrid.tsx
+++ src/components/PokemonCardGrid.tsx
@@ -3,10 +3,14 @@ import { pokemonTypeInterface, userPokemonsType } from "../utils/Types";
 import { IoGitCompare } from "react-icons/io5";
 import { FaPlus, FaTrash } from "react-icons/fa";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../app/hooks";
+import { addToCompare } from "../app/slices/PokemonSlice";
+import { setToast } from "../app/slices/AppSlice";
 
 function PokemonCardGrid({ pokemons }: { pokemons: userPokemonsType[] }) {
   const location = useLocation();
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
   return (
     <div className="pokemon-card-grid-container">
       <div className="pokemon-card-grid">
@@ -25,7 +29,16 @@ function PokemonCardGrid({ pokemons }: { pokemons: userPokemonsType[] }) {
                   )}
                 </div>
                 <div className="pokemon-card-compare">
-                  <IoGitCompare />
+                  <IoGitCompare
+                    onClick={() => {
+                      dispatch(addToCompare(data));
+                      dispatch(
+                        setToast(
+                          `${data.name} has been added to Compare Queue.`
+                        )
+                      );
+                    }}
+                  />
                 </div>
                 <h3 className="pokemon-card-title">{data.name}</h3>
                 <img
